9f77990471fc724a5b8f9d781be9363af38a74ac
zafaruralov <sultonovzafar007@gmail.com>
2023-02-21 01:46:06 +0500

    ready to use

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   });
 
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://ieltszone.netlify.app/");
     next();
   });
   const corsOptions = {
